f8d34420fdc14e71f85c8f5973c746a9cbc5e344
Christelle-Labin <labin.christelle@gmail.com>
2024-10-22 00:12:36 +0200

    Fonction fléchéee + argument // Boucle for

---
+++ steps-2.js
@@ -0,0 +1,27 @@
+/*
+    Dans cet exercice vous devrez appeler la fonction `showCurrentStep` plusieurs fois.
+    Vous devrez afficher dans le terminal :
+
+    Etape 1
+    Etape 2
+    Etape 3
+    Etape 4
+    Etape 5
+
+*/
+
+console.log("Etape 1");
+
+const showCurrentStep = step => {
+    console.log("Etape " + step);
+};
+
+// Début de votre code
+
+showCurrentStep (2);
+showCurrentStep (3);
+showCurrentStep (4);
+
+// Fin de votre code
+
+console.log("Etape 5");
