ed7ace26728c81a0dd0f6e36b8621ae97634d002
Christelle-Labin <labin.christelle@gmail.com>
2024-10-22 10:39:13 +0200

    Fonction fléchée + return number true or false

---
+++ positiveNegative.js
@@ -0,0 +1,19 @@
+//Méthode 1  :
+const isPositive = (number) => {
+    // Début de votre code
+   
+   return number >= 0
+    // Fin de votre code
+}
+
+console.log(isPositive(-4)); // devrait afficher false
+console.log(isPositive(7)); // devrait afficher true
+
+
+// Méthode 2 :
+
+// if (number >= 0) {
+//     return true
+// } else {
+//     return false
+// }
