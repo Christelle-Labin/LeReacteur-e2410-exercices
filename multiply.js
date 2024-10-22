d5b5368c687c8cffacec85208b2661d9e5a91125
Christelle-Labin <labin.christelle@gmail.com>
2024-10-22 10:09:32 +0200

    Fonction fléchée + return

---
+++ multiply.js
@@ -0,0 +1,10 @@
+// Début de votre code
+const multiply = (a,b) => {
+ return a * b;
+}
+
+// Fin de votre code
+
+console.log(multiply(2,3)); // Doit afficher 6
+console.log(multiply(4,-2)); // Doit afficher -8
+
