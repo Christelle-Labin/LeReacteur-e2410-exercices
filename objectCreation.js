0ca392a5dd41b6f4f97c2b3c623c56deb00f5653
Christelle-Labin <labin.christelle@gmail.com>
2024-11-01 22:49:28 +0100

    Objet + clé valeur

---
+++ objectCreation.js
@@ -0,0 +1,17 @@
+// Créez un objet appelé person avec les propriétés suivantes :
+
+// firstName qui renverra une chaîne de caractères
+// lastName qui renverra une chaîne de caractères
+// age qui renverra un nombre entier 
+// Assignez des valeurs à chaque propriété, puis affichez-les dans la console.
+
+
+// Début de votre code
+const person = {
+firstName: "John",
+lastname: "Doe",
+age: 35,
+};
+// Fin de votre code
+
+console.log(person); // Devra afficher par exemple : { firstName: "John", lastName: "Doe", age: 32 }
